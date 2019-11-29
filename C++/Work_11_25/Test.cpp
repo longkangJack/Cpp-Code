14635caa28039aeb4e1fa10859a2c63f95a0a5e7
longkangjack <1030269471@qq.com>
2019-11-29 21:41:13 +0800

    更新学习

--- C++/Work_11_25/Test.cpp
+++ C++/Work_11_25/Test.cpp
@@ -1,8 +1,158 @@
 #include<iostream>
-#include<stdio.h>
+#include<string.h>
 #include"fun.h"
 using namespace std; //
+class String
+{
+public:
+	//深拷贝
+	String(const String &s)
+	{
+		m_data = (char*)malloc(strlen(s.m_data)+1);
+		strcpy(m_data, s.m_data);
+	}
+	~String()
+	{
+		free(m_data);
+	}
+public:
+	void show()const // show(const * const this)
+	{
 
+	}
+private:
+	char* m_data;
+};
+void main()
+{
+
+}
+//int main()
+//{
+//	short (*arr)[10][10];
+//	short *arr[10][10];
+//	for (int i = 0; i < 5; i++)
+//	{
+//		arr[i] = 'a' + 1;
+//	}//赋值 strlen 结果是随机值
+//	初始化的话 如果空间没有被初始化完全 其他用0补充所以就是5
+//	cout << strlen(arr) << endl;
+//	cout << sizeof(arr) << endl;
+//	return 0;
+//}
+/*class Test
+{
+public:
+	Test()
+	{
+		m_data = 0;
+	}
+private:
+	int m_data;
+};
+void main()
+{
+	Test t;
+	Test t1 = t;
+}
+//1 常方法的 const 怎么了
+//不能修改数据成员 //未初始化完空间 基本按照0补齐
+//2 GetData() 与GetData()const 能否共存可以共存
+//可以共存
+//3 拷贝构造函数的const
+//***\* const\******* \* ：\****
+//*** \* 为了整个程序的安全，还是加上const，防止对引用类型参数值的意外修改。\****
+//*** \*& \******* \* ：\****
+//*** \* 如果传值，就必须要创建一个临时对象，创建对象就要调用拷贝构造函数，这样，就会无穷递归调用。(编译器强制性要求参数必须要用&)\****
+//4 赋值语句
+//Test& operator = (const Test &t)
+//void operator = (Test t)
+//面向对象
+//efficient
+
+/*
+class Test
+{
+public://系统存在默认构造函数 写上之后 默认的构造函数就失去了作用
+	//名字和类相同
+	//无返回值
+	//可以重载
+	Test(int data)
+	{
+		cout << "Create Test Object:" << this << endl;
+		m_data = data;
+	}
+	//拷贝构造函数
+	Test(const Test& t)
+	{
+		cout << "Copy Test Object" << this << endl;
+		m_data = t.m_data;
+	}
+	//析构函数
+	//不能重载
+	//是构造函数的取反
+	//一个类只能有一个析构函数
+	~Test()
+	{
+		cout << "Free Test Object:" << this << endl;
+	}
+	//赋值语句
+	Test& operator = (Test t)
+	{
+
+	}
+	//常方法
+public:
+	void show()
+	{
+		cout << m_data << endl;
+	}
+	//int GetData(const int * const  this)
+	int GetData()const
+	{
+		return m_data;
+	}
+	//int GetData( int* const  this)
+	int GetData()
+	{
+		return m_data;
+	}
+private:
+	int m_data = 0;
+};
+void main()
+{
+	/*Test t(10);//普通初始对象
+	Test t1 = t;//对象初始化对象  //先调用拷贝构造函数
+	Test t2(100);	*/
+	/*Test t(10);
+	t.show();
+}*/
+/*class Test
+{
+public:
+	Test()//构造函数自动调动   必须与类的名字相同
+	{
+		cout << "creat test object" << endl;
+	}
+	Test(int data)
+	{
+		m_data = data;
+		cout << m_data << endl;
+	}
+private:
+	int m_data;
+	int m_value;
+};
+void main()
+{
+	Test t;
+	Test t1(10);
+}
+/*void main()
+{
+	cout << "The type is " << sizeof(int) << endl;
+}
 
 /*int main()
 {
